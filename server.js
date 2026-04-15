75147b9668b32b358426658d927a4f4fd31391e6
Ravikeerthy <ravikeerthy1807@gmail.com>
2026-04-15 14:12:37 +0400

    cors changed

--- server.js
+++ server.js
@@ -15,7 +15,14 @@ dotenv.config()
 
 const app = express();
 
-app.use(cors())
+app.use(cors(
+    {
+        origin:[
+            "http://localhost:5173"
+        ],
+        credentials:true,
+    }
+))
 app.use(express.json())
 
 app.use("/api/auth", authRoutes)
